699b24f7ab233fb404a8327aca29e74fa58b7cb4
AlvineMoses <65952407+AlvineMoses@users.noreply.github.com>
2025-05-15 10:35:24 +0300

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Otp from "./pages/Otp";
 // import Nav from "./components/Nav/Nav";
 import "./App.css";
